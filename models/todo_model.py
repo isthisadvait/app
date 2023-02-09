2d1f4c95925864c06c603f18c82a2a6eda9640e8
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2023-02-09 11:48:48 +0530

    Update todo_model.py

--- models/todo_model.py
+++ models/todo_model.py
@@ -1,34 +1,36 @@
 from typing import Optional
-from beanie import Document,Indexed,Link,before_event,Replace,Insert
-from uuid import UUID,uuid4
-from pydantic import Field,EmailStr
 from datetime import datetime
+from uuid import UUID, uuid4
+from beanie import Document, Indexed, Link, before_event, Replace, Insert
+from pydantic import Field
 from .user_model import User
 
 class Todo(Document):
-    todo_id: UUID = Field(default_factory=uuid4,unique = True)
-    status:bool = False
+    todo_id: UUID = Field(default_factory=uuid4, unique=True)
+    status: bool = False
     title: Indexed(str)
-    created_at: datetime = Field(default_factory=datetime.utcnow())
-    updated_at:datetime = Field(default_factory=datetime.utcnow())
-    owner = Link[User]
-
+    description: str = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    owner: Link[User]
+    
     def __repr__(self) -> str:
         return f"<Todo {self.title}>"
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return self.title
 
     def __hash__(self) -> int:
         return hash(self.title)
 
-    def __eq__(self,other:object) ->bool:
-        if isinstance(other,Todo):
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Todo):
             return self.todo_id == other.todo_id
         return False
-    @before_event(Replace,Insert)
+    
+    @before_event([Replace, Insert])
     def update_update_at(self):
         self.updated_at = datetime.utcnow()
-    
+        
     class Settings:
         name = 'todo'
