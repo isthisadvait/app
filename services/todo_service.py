a498cf4176ba5a00c1650956694889db8373c5d3
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2023-02-09 11:40:58 +0530

    Update todo_service.py
    added UserUpdate

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,21 +1,49 @@
-from typing import List
-from app.models.user_model import User
-from app.models.todo_model import Todo
-from app.schemas.todo_schema import TodoCreate
+from typing import Optional
 from uuid import UUID
-class TodoService:
-    @staticmethod
-    async def list_todos(user:User)->List[Todo]:
-        todos = await Todo.find(Todo.owner.id == user.user_id).to_list()
-        return todos
+from app.schemas.user_schema import UserAuth
+from app.models.user_model import User
+from app.core.security import get_password, verify_password
+import pymongo
 
-    @staticmethod
-    async def create_todo(user:User,data:TodoCreate):
-        todo = Todo(**data.dict(),owner = user)
-        return await todo.insert()
+from app.schemas.user_schema import UserUpdate
 
-    @staticmethod
-    async def retrieve_todo(current_user:User,todo_id:UUID):
-        todo = await Todo.find_one(Todo.todo_id == todo_id,Todo.owner.id == current_user.id)
-        return todo
 
+class UserService:
+    @staticmethod
+    async def create_user(user: UserAuth):
+        user_in = User(
+            username=user.username,
+            email=user.email,
+            hashed_password=get_password(user.password)
+        )
+        await user_in.save()
+        return user_in
+    
+    @staticmethod
+    async def authenticate(email: str, password: str) -> Optional[User]:
+        user = await UserService.get_user_by_email(email=email)
+        if not user:
+            return None
+        if not verify_password(password=password, hashed_pass=user.hashed_password):
+            return None
+        
+        return user
+    
+    @staticmethod
+    async def get_user_by_email(email: str) -> Optional[User]:
+        user = await User.find_one(User.email == email)
+        return user
+    
+    @staticmethod
+    async def get_user_by_id(id: UUID) -> Optional[User]:
+        user = await User.find_one(User.user_id == id)
+        return user
+    
+    @staticmethod
+    async def update_user(id: UUID, data: UserUpdate) -> User:
+        user = await User.find_one(User.user_id == id)
+        if not user:
+            raise pymongo.errors.OperationFailure("User not found")
+    
+        await user.update({"$set": data.dict(exclude_unset=True)})
+        return user
