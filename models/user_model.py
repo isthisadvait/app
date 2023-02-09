ff617f4d73e00231838f1429d38cb40f7d91f116
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2023-02-09 10:53:35 +0530

    Add files via upload

---
+++ models/user_model.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from beanie import Document,Indexed
+from uuid import UUID,uuid4
+from pydantic import Field,EmailStr
+from datetime import datetime
+class User(Document):
+    user_id:UUID = Field(default_factory=uuid4)
+    username:  Indexed(str,unique = True)
+    email:Indexed(EmailStr,unique= True)
+    hashed_password : str
+    first_name:Optional[str]=None
+    last_name: Optional[str]=None
+    disabled:Optional[bool]=False
+
+    def __repr__(self) -> str:
+        return f"<User {self.email}>"
+
+    def __str__(self)->str:
+        return self.email
+
+    def __hash__(self) -> int:
+        return hash(self.email)
+
+    def __eq__(self,other:object) ->bool:
+        if isinstance(other,User):
+            return self.email == other.email
+        return False
+
+    @property
+    def create(self) -> datetime:
+        return self.id.generation_time
+
+    @classmethod 
+    async def by_email(self , email:str):
+        return await self.find_one(self.email == email)
+
+    class Settings:
+        name = 'users'
