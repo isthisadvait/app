022910fdf935ec6893a33890f876fed42c9d4a0a
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2023-02-09 11:45:12 +0530

    Update user.py

--- api/app_v1/handlers/user.py
+++ api/app_v1/handlers/user.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter,HTTPException,status
-from app.schemas.user_schema import UserAuth,UserOut
+from fastapi import APIRouter, HTTPException, status
+from app.schemas.user_schema import UserAuth, UserOut, UserUpdate
+from fastapi import Depends
 from app.services.user_service import UserService
 import pymongo
+from app.models.user_model import User
+from app.api.deps.user_deps import get_current_user
 user_router = APIRouter()
 
 @user_router.post('/create',summary="Create new user",response_model=UserOut)
@@ -12,4 +15,19 @@ async def create_user(data:UserAuth):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User with this email or username already exist"
+        )
+
+@user_router.get('/me', summary='Get details of currently logged in user', response_model=UserOut)
+async def get_me(user: User = Depends(get_current_user)):
+    return user
+
+
+@user_router.post('/update', summary='Update User', response_model=UserOut)
+async def update_user(data: UserUpdate, user: User = Depends(get_current_user)):
+    try:
+        return await UserService.update_user(user.user_id, data)
+    except pymongo.errors.OperationFailure:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User does not exist"
         )
